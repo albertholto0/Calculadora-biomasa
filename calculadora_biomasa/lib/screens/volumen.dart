030c1103890a23c306599567b9dab574493ec568
Albert Contreras <albertmendoza019@gmail.com>
2025-06-12 22:01:12 -0600

    Se hicieron cambios siguiendo las recomendaciones de pablo

--- calculadora_biomasa/lib/screens/volumen.dart
+++ calculadora_biomasa/lib/screens/volumen.dart
@@ -15,6 +15,7 @@ class _VolumenCalculatorState extends State<VolumenCalculator> {
   final TextEditingController _heigthController = TextEditingController();
 
   String results = '';
+  String alerta = '';
 
   void _calculateVolumen() {
     final diametro = double.tryParse(_diameterController.text);
@@ -23,6 +24,22 @@ class _VolumenCalculatorState extends State<VolumenCalculator> {
     if (diametro == null || altura == null || diametro <= 0 || altura <= 0) {
       alertScreen(context, 'Error', 'Por favor ingresa valores válidos.');
       return;
+    }
+
+    if (diametro >= 400 && altura > 60) {
+      alertScreen(
+        context,
+        "Alerta",
+        "Verifica si el diametro ingresado es el correcto. Además, es dificil encontrar arboles con 60 metros de alto. Verifica si la altura es la correcta",
+      );
+    }
+    if (diametro >= 400) {
+      alerta = "Verifica si el diametro ingresado es el correcto";
+    } else {
+      if (altura > 60) {
+        alerta =
+            "Es dificil encontrar arboles con 60 metros de alto. Verifica si la altura es la correcta";
+      }
     }
 
     double a, b, c;
@@ -40,6 +57,11 @@ class _VolumenCalculatorState extends State<VolumenCalculator> {
       b = 1.871412;
       c = 0.828973;
     } else {
+      alertScreen(
+        context,
+        "Error",
+        "El diametro debe ser mayor o igual a 7.5 cm",
+      );
       setState(() {
         results = "El diametro debe ser mayor o igual a 7.5 cm";
       });
@@ -53,6 +75,9 @@ class _VolumenCalculatorState extends State<VolumenCalculator> {
     });
   }
 
+  // Si supera 400 CM verificar si la medida es la correcta para el diametro normal.
+  // Para el diametro basal igual
+  // En altura es dificil encontrar arboles con 60 metros de alto. Si supera esta medida verificar si la altura es la correcta
   @override
   Widget build(BuildContext context) {
     return Scaffold(
